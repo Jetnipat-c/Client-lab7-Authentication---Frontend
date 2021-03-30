069c38232dc9a6183d7fa2bbebe8d2704fcd5d30
alonecheer <alonecheer@hotmail.com>
2021-03-31 04:15:16 +0700

    :zap: sigup

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,29 @@
-export default function Home() {
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/router";
+const signup = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [age, setAge] = useState<number>();
+  const [username, setUsername] = useState<string>("");
+  const handleSigup = async (event) => {
+    event.preventDefault();
+    let result = await axios.post("http://localhost:99/api/register", {
+      username,
+      password,
+      age,
+      email,
+    });
+    console.log("res", result.data);
+    if (result.data.message === "Register success") {
+      router.push("/")
+    } else if (result.data.status === "Duplicated user") {
+      alert("Duplicated user")
+    } else {
+        alert("Error")
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -9,13 +34,28 @@ export default function Home() {
             alt="Workflow"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in
+            Sign up
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600"></p>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form className="mt-8 space-y-6" onSubmit={handleSigup}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
             <div>
               <label htmlFor="email-address" className="sr-only">
                 Email address
@@ -28,6 +68,7 @@ export default function Home() {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
@@ -42,34 +83,37 @@ export default function Home() {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Age
+              </label>
+              <input
+                id="age"
+                name="age"
+                type="number"
+                autoComplete="age"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Age"
+                onChange={(e) => setAge(parseInt(e.target.value))}
               />
             </div>
           </div>
 
           <div className="flex items-center justify-between">
             <div className="text-sm">
-              <span className="pr-2">Don't have an account?</span>
+              <span className="pr-2">Have an account?</span>
               <a
-                href="/signup"
+                href="/"
                 className="font-medium text-indigo-600 hover:text-indigo-500"
               >
-                Sign up
+                Sign in
               </a>
             </div>
-            <div className="flex items-center">
-              <input
-                id="remember_me"
-                name="remember_me"
-                type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-              />
-              <label
-                htmlFor="remember_me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
-              </label>
-            </div>
+            <div className="flex items-center"></div>
           </div>
 
           <div>
@@ -86,17 +130,18 @@ export default function Home() {
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </span>
-              Sign in
+              Sign up
             </button>
           </div>
         </form>
       </div>
     </div>
   );
-}
+};
+export default signup;
